871f5607587761804e236fdfba7dd8d80305cf9f
Peggy&Ryuan <potm@163.com>
2015-08-06 18:58:22 +0800

    fix little

--- app/src/main/java/com/qingchengfit/fitcoach/fragment/CompleteInfoFragment.java
+++ app/src/main/java/com/qingchengfit/fitcoach/fragment/CompleteInfoFragment.java
@@ -8,15 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.paper.paperbaselibrary.utils.LogUtil;
 import com.qingchengfit.fitcoach.R;
-import com.qingchengfit.fitcoach.http.QcCloudClient;
-import com.qingchengfit.fitcoach.http.bean.RegisteBean;
-import com.qingchengfit.fitcoach.http.bean.ResponseResult;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import rx.schedulers.Schedulers;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,11 +21,6 @@ public class CompleteInfoFragment extends Fragment {
 
     @Bind(R.id.comple_name)
     TextView compleName;
-
-    public CompleteInfoFragment(String code) {
-        // Required empty public constructor
-
-    }
 
     public CompleteInfoFragment() {
     }
