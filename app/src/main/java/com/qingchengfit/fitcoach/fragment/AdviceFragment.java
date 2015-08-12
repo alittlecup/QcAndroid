6a361c11678e77271cba003331372eca09fb010e
Peggy&Ryuan <potm@163.com>
2015-08-12 16:44:29 +0800

    add radio selector

--- app/src/main/java/com/qingchengfit/fitcoach/fragment/AdviceFragment.java
+++ app/src/main/java/com/qingchengfit/fitcoach/fragment/AdviceFragment.java
@@ -3,17 +3,32 @@ package com.qingchengfit.fitcoach.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.paper.paperbaselibrary.utils.LogUtil;
 import com.qingchengfit.fitcoach.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import rx.Observable;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AdviceFragment extends Fragment {
 
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.setting_advice_mail)
+    EditText settingAdviceMail;
+    @Bind(R.id.setting_advice_content)
+    EditText settingAdviceContent;
 
     public AdviceFragment() {
         // Required empty public constructor
@@ -24,8 +39,27 @@ public class AdviceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_advice, container, false);
+        View view = inflater.inflate(R.layout.fragment_advice, container, false);
+        ButterKnife.bind(this, view);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_left);
+        toolbar.setTitle("意见反馈");
+        toolbar.setNavigationOnClickListener(view1 -> LogUtil.e("back"));
+        return view;
     }
 
+    @OnClick(R.id.setting_advice_btn)
+    public void onAdvice() {
+        Observable.just("")
+                .subscribe(s ->
+                                LogUtil.e("advice")
+                )
 
+        ;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
