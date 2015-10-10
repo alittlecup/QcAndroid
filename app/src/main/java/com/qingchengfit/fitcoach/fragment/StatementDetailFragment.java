044696f869e2758ab5a1a4c3e46563d5680a8205
Peggy&Ryuan <potm@163.com>
2015-10-10 17:04:43 +0800

    statement native

---
+++ app/src/main/java/com/qingchengfit/fitcoach/fragment/StatementDetailFragment.java
@@ -0,0 +1,32 @@
+package com.qingchengfit.fitcoach.fragment;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qingchengfit.fitcoach.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StatementDetailFragment extends Fragment {
+    public static final String TAG = StatementDetailFragment.class.getName();
+
+
+    public StatementDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_statement_detail, container, false);
+    }
+
+
+}
