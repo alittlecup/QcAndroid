db2b7fd31e723c6c60587f004a7c102fe01c2983
Peggy&Ryuan <potm@163.com>
2015-12-07 21:16:27 +0800

    add: GalleryPotoView

--- app/src/main/java/com/qingchengfit/fitcoach/adapter/ViewPaperAdapter.java
+++ app/src/main/java/com/qingchengfit/fitcoach/adapter/ViewPaperAdapter.java
@@ -1,11 +1,11 @@
 package com.qingchengfit.fitcoach.adapter;
 
 
-import java.util.List;
-
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+
+import java.util.List;
 
  
 public class ViewPaperAdapter extends PagerAdapter {
