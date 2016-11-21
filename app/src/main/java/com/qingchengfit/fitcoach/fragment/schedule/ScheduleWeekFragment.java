f925a82ffdc6051c967d960407fd88d59cb4f20e
Paper <potm@163.com>
2016-11-21 13:38:42 +0800

    Add sync pag

---
+++ app/src/main/java/com/qingchengfit/fitcoach/fragment/schedule/ScheduleWeekFragment.java
@@ -0,0 +1,37 @@
+package com.qingchengfit.fitcoach.fragment.schedule;
+
+import cn.qingchengfit.staffkit.constant.BaseFragment;
+
+/**
+ * power by
+ * MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+ * MM.:  .:'   `:::  .:`MMMMMMMMMMM|`MMM'|MMMMMMMMMMM':  .:'   `:::  .:'.MM
+ * MMMM.     :          `MMMMMMMMMM  :*'  MMMMMMMMMM'        :        .MMMM
+ * MMMMM.    ::    .     `MMMMMMMM'  ::   `MMMMMMMM'   .     ::   .  .MMMMM
+ * MMMMMM. :   :: ::'  :   :: ::'  :   :: ::'      :: ::'  :   :: ::.MMMMMM
+ * MMMMMMM    ;::         ;::         ;::         ;::         ;::   MMMMMMM
+ * MMMMMMM .:'   `:::  .:'   `:::  .:'   `:::  .:'   `:::  .:'   `::MMMMMMM
+ * MMMMMM'     :           :           :           :           :    `MMMMMM
+ * MMMMM'______::____      ::    .     ::    .     ::     ___._::____`MMMMM
+ * MMMMMMMMMMMMMMMMMMM`---._ :: ::'  :   :: ::'  _.--::MMMMMMMMMMMMMMMMMMMM
+ * MMMMMMMMMMMMMMMMMMMMMMMMMM::.         ::  .--MMMMMMMMMMMMMMMMMMMMMMMMMMM
+ * MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM-.     ;::-MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+ * MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM. .:' .M:F_P:MMMMMMMMMMMMMMMMMMMMMMMMMMM
+ * MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.   .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+ * MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM\ /MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+ * MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMVMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+ * Created by Paper on 16/11/21.
+ */
+public class ScheduleWeekFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_, container, false);
+        return view;
+    }
+
+    @Override
+    public String getFragmentName() {
+        return ScheduleWeekFragment.class.getName();
+    }
+}
