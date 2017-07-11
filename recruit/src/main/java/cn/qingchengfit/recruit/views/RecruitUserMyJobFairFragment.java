b786931183f6e8dcd1cfbc75adf26c965dfdb8c1
Paper <potm@163.com>
2017-07-11 13:29:44 +0800

    remove iml

--- recruit/src/main/java/cn/qingchengfit/recruit/views/RecruitUserMyJobFairFragment.java
+++ recruit/src/main/java/cn/qingchengfit/recruit/views/RecruitUserMyJobFairFragment.java
@@ -1,5 +1,16 @@
 package cn.qingchengfit.recruit.views;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import butterknife.ButterKnife;
+import cn.qingchengfit.recruit.R;
+
 /**
  * power by
  * MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
@@ -22,6 +33,24 @@ package cn.qingchengfit.recruit.views;
  */
 
 public class RecruitUserMyJobFairFragment extends JobFairListFragment {
+  protected Toolbar toolbar;
+  protected TextView toolbarTitle;
+  @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View view = super.onCreateView(inflater, container, savedInstanceState);
+    LinearLayout linearLayout = (LinearLayout) inflater.inflate(R.layout.layout_toolbar_container,null);
+    linearLayout.addView(view,1);
+    toolbar = ButterKnife.findById(linearLayout,R.id.toolbar);
+    toolbarTitle = ButterKnife.findById(linearLayout,R.id.toolbar_title);
+    initToolbar(toolbar);
+    return linearLayout;
+  }
+
+  @Override public void initToolbar(@NonNull Toolbar toolbar) {
+    super.initToolbar(toolbar);
+    toolbarTitle.setText("我参加的专场招聘会");
+  }
+
   @Override public void onRefresh() {
     presenter.queryUserMyJobFairs();
   }
