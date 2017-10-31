e26fc749a10fd68e6fc1b71b971d56db677848c4
fb0122 <fb0122@163.com>
2017-10-31 19:43:59 +0800

    pos机部分修改

--- saasbase/src/main/java/cn/qingchengfit/saasbase/bill/items/ItemBill.java
+++ saasbase/src/main/java/cn/qingchengfit/saasbase/bill/items/ItemBill.java
@@ -42,7 +42,7 @@ public class ItemBill extends AbstractFlexibleItem<ItemBill.ItemBillVH> {
       List payloads) {
 
     holder.imgBill.setImageResource(bill.getTradeDrawable(bill.pay_type));
-    holder.tvTime.setText(DateUtils.getTimeHHMM(DateUtils.formatDateFromServer(bill.pay_time)));
+    holder.tvTime.setText(DateUtils.Date2MMDDHHmm(DateUtils.formatDateFromServer(bill.pay_time)));
     holder.tvOperator.setText(holder.itemView.getContext()
         .getResources()
         .getString(R.string.bill_operator, bill.created_by.username));
