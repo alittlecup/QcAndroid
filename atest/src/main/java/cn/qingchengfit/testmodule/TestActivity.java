6a18cc7c81d11acea6b2aee0a9bedb7e73e8ab01
huangbaole <huangbaole@qingcheng.it>
2017-11-12 20:04:59 +0800

    修改atest便于测试

--- atest/src/main/java/cn/qingchengfit/testmodule/TestActivity.java
+++ atest/src/main/java/cn/qingchengfit/testmodule/TestActivity.java
@@ -1,10 +1,13 @@
 package cn.qingchengfit.testmodule;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.Observable;
+import android.databinding.ObservableField;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class TestActivity extends AppCompatActivity {
-
+    public ObservableField<String> textfile=new ObservableField<>("1111");
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
