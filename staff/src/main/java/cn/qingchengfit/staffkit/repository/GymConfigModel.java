61fc6a1a9401633d23672b2546f124f95fa9adaf
Paper <potm@163.com>
2017-12-01 17:55:21 +0800

    mig course staff site

--- staff/src/main/java/cn/qingchengfit/staffkit/repository/GymConfigModel.java
+++ staff/src/main/java/cn/qingchengfit/staffkit/repository/GymConfigModel.java
@@ -2,6 +2,7 @@ package cn.qingchengfit.staffkit.gym;
 
 import cn.qingchengfit.di.model.GymWrapper;
 import cn.qingchengfit.di.model.LoginStatus;
+import cn.qingchengfit.network.QcRestRepository;
 import cn.qingchengfit.network.response.QcDataResponse;
 import cn.qingchengfit.saasbase.gymconfig.IGymConfigModel;
 import cn.qingchengfit.saasbase.gymconfig.network.response.SpaceListWrap;
@@ -32,10 +33,12 @@ public class GymConfigModel implements IGymConfigModel {
 
   GymWrapper gymWrapper;
   LoginStatus loginStatus;
+  
 
-  public GymConfigModel(GymWrapper gymWrapper, LoginStatus loginStatus , ) {
+  public GymConfigModel(GymWrapper gymWrapper, LoginStatus loginStatus , QcRestRepository qcRestRepository) {
     this.gymWrapper = gymWrapper;
     this.loginStatus = loginStatus;
+
   }
 
   @Override public Observable<QcDataResponse<SpaceListWrap>> getSites(int type) {
