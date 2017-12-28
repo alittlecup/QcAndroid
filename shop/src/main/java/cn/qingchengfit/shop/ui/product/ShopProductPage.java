8203da1d7d8a204984ee1aa03220ee99560b5722
huangbaole <huangbaole@qingcheng.it>
2017-12-28 11:21:20 +0800

    修改图片选择库为私有maven中的，更新添加商品型号UI

--- shop/src/main/java/cn/qingchengfit/shop/ui/product/ShopProductPage.java
+++ shop/src/main/java/cn/qingchengfit/shop/ui/product/ShopProductPage.java
@@ -3,15 +3,16 @@ package cn.qingchengfit.shop.ui.product;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import com.anbillon.flabellum.annotations.Leaf;
 import cn.qingchengfit.shop.base.ShopBaseFragment;
 import cn.qingchengfit.shop.databinding.PageShopProductBinding;
+import cn.qingchengfit.shop.ui.widget.CategoryItemView;
+import com.anbillon.flabellum.annotations.Leaf;
 
 /**
  * Created by huangbaole on 2017/12/18.
  */
-@Leaf(module = "shop",path = "/shop/product")
-public class ShopProductPage extends ShopBaseFragment<PageShopProductBinding,ShopProductViewModel>{
+@Leaf(module = "shop", path = "/shop/product") public class ShopProductPage
+    extends ShopBaseFragment<PageShopProductBinding, ShopProductViewModel> {
   @Override protected void subscribeUI() {
 
   }
@@ -19,12 +20,29 @@ public class ShopProductPage extends ShopBaseFragment<PageShopProductBinding,Sho
   @Override
   public PageShopProductBinding initDataBinding(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    mBinding=PageShopProductBinding.inflate(inflater,container,false);
+    mBinding = PageShopProductBinding.inflate(inflater, container, false);
     initViewPager();
+    initCategoryView();
     return mBinding;
   }
 
+  private void initCategoryView() {
+    mBinding.addProductCategory.setOnClickListener(view -> {
+      CategoryItemView childAt = (CategoryItemView) mBinding.llCategoryContainer.getChildAt(0);
+      if (mBinding.llCategoryContainer.getChildCount() == 1 && !childAt.isCategoryVisible()) {
+        childAt.setCategoryVisible(true);
+        childAt.setDeleteVisible(true);
+      } else {
+        mBinding.llCategoryContainer.addView(new CategoryItemView(getContext()));
+      }
+    });
+    CategoryItemView categoryItemIView = new CategoryItemView(getContext());
+    categoryItemIView.setCategoryVisible(false);
+    categoryItemIView.setDeleteVisible(false);
+    mBinding.llCategoryContainer.addView(categoryItemIView);
+  }
+
   private void initViewPager() {
-    
+
   }
 }
