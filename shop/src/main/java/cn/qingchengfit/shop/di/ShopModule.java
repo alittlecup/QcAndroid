b32d493cfd4352673ab0adbebf902391a1eb6266
huangbaole <huangbaole@qingcheng.it>
2018-02-26 10:04:09 +0800

    update change

--- shop/src/main/java/cn/qingchengfit/shop/di/ShopModule.java
+++ shop/src/main/java/cn/qingchengfit/shop/di/ShopModule.java
@@ -1,17 +1,13 @@
 package cn.qingchengfit.shop.di;
 
-import dagger.Module;
 import dagger.Subcomponent;
-import javax.inject.Singleton;
 
 /**
  * Created by huangbaole on 2017/12/19.
  */
-@Module(subcomponents = { ShopModule.ShopModelComponent.class }) public abstract class ShopModule {
-  @Singleton @Subcomponent(modules = { BindShopActivity.class, ShopViewModel.class })
-  interface ShopModelComponent {
-    @Subcomponent.Builder interface Builder {
-      ShopModelComponent build();
-    }
+ @Subcomponent(modules = { BindShopActivity.class, ShopViewModel.class })
+public interface ShopModelComponent {
+  @Subcomponent.Builder interface Builder {
+    ShopModelComponent build();
   }
 }
