5e01f9f023d9b2996e17cff4be75c0ea73dbb544
huangbaole <huangbaole@qingcheng.it>
2018-03-01 17:21:56 +0800

    修改StaffStudentActivity移动到StudentActivity

--- staff/src/main/java/cn/qingchengfit/staffkit/student/StaffStudentActivity.java
+++ staff/src/main/java/cn/qingchengfit/staffkit/student/StaffStudentActivity.java
@@ -1,17 +1,13 @@
 package cn.qingchengfit.staffkit.student;
 
 import cn.qingchengfit.saasbase.SaasContainerActivity;
-import cn.qingchengfit.saasbase.staff.views.StaffAddFragment;
-import cn.qingchengfit.saasbase.student.views.ChooseAndSearchStudentFragment;
-import cn.qingchengfit.saasbase.student.views.StudentAddFragment;
 import com.anbillon.flabellum.annotations.Trunk;
 
 /**
  * Created by fb on 2017/12/18.
  */
 
-@Trunk(fragments = {ChooseAndSearchStudentFragment.class, StaffAddFragment.class,
-    StudentAddFragment.class, })
+@Trunk(fragments = { })
 public class StaffStudentActivity extends SaasContainerActivity {
   @Override public String getModuleName() {
     return "student";
