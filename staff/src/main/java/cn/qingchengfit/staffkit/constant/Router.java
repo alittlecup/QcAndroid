72fc3f219276be86934b0d5175fc2c0827015324
huangbaole <huangbaole@qingcheng.it>
2018-04-13 11:30:53 +0800

    如何使用修改成https

--- staff/src/main/java/cn/qingchengfit/staffkit/constant/Router.java
+++ staff/src/main/java/cn/qingchengfit/staffkit/constant/Router.java
@@ -22,9 +22,9 @@ package cn.qingchengfit.staffkit.constant;
  */
 
 public class Router {
-    public static final String WEB_HOW_TO_USE = "http://cloud.qingchengfit.cn/mobile/urls/885f339caa124675a36a17a8ca4977d5/";
-    public static final String WEB_HOW_TO_USE_SIGNIN = "http://cloud.qingchengfit.cn/mobile/urls/96904e55171146ed909ea4aeb90e5ead";
-    public static final String WEB_HOW_TO_USE_BATCH_GROUP = "http://cloud.qingchengfit.cn/mobile/urls/e382d87968dd4f54a89bb5e5a933f779/";
+    public static final String WEB_HOW_TO_USE = "https://cloud.qingchengfit.cn/mobile/urls/885f339caa124675a36a17a8ca4977d5/";
+    public static final String WEB_HOW_TO_USE_SIGNIN = "https://cloud.qingchengfit.cn/mobile/urls/96904e55171146ed909ea4aeb90e5ead";
+    public static final String WEB_HOW_TO_USE_BATCH_GROUP = "https://cloud.qingchengfit.cn/mobile/urls/e382d87968dd4f54a89bb5e5a933f779/";
     //团课
     public static final String WEB_HOW_TO_USE_BATCH_PRIVATE = "http://cloud.qingchengfit.cn/mobile/urls/34890304d8bc40ba9677ca8d99bcd02a/";
     //
