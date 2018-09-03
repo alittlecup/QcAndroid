927f39519d3ad528712337206b626ddc4eef3c58
huangbaole <huangbaole@qingcheng.it>
2018-09-03 14:59:46 +0800

    fix：跟进记录列表

--- staff/src/main/java/cn/qingchengfit/staffkit/views/student/detail/StudentHomeFragment.java
+++ staff/src/main/java/cn/qingchengfit/staffkit/views/student/detail/StudentHomeFragment.java
@@ -267,9 +267,8 @@ public class StudentHomeFragment extends BaseFragment implements View.OnClickLis
     super.onFinishAnimation();
     fragments.add(new ClassRecordFragment());
     fragments.add(new StudentsCardsFragment());
-    //FollowRecordPage followRecordPage = new FollowRecordPage();
-    //fragments.add(followRecordPage);
-    fragments.add(new FollowRecordFragment());
+    FollowRecordPage followRecordPage = new FollowRecordPage();
+    fragments.add(followRecordPage);
     fragments.add(new StudentMoreInfoFragment());
     mAdapter.notifyDataSetChanged();
     switch (statusToTab) {
