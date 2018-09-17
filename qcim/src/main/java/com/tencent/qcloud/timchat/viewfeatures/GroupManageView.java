b59044576343139bcdb5906ff2524a40a27064e8
huangbaole <huangbaole@qingcheng.it>
2018-09-17 18:03:56 +0800

    去除合并中无用代码

--- qcim/src/main/java/com/tencent/qcloud/timchat/viewfeatures/GroupManageView.java
+++
@@ -1,9 +0,0 @@
-package com.tencent.qcloud.timchat.viewfeatures;
-
-/**
- * 群管理接口
- */
-public interface GroupManageView {
-
-
-}
--- qcim/src/main/java/com/tencent/qcloud/timchat/viewfeatures/MvpView.java
+++
@@ -1,8 +0,0 @@
-package com.tencent.qcloud.timchat.viewfeatures;
-
-/**
- * Created by admin on 16/3/1.
- */
-public interface MvpView {
-
-}
