6a5f23ba4030eb1cb3d01bce4d1408cad05677a3
huangbaole <huangbaole@qingcheng.it>
2018-11-07 17:27:18 +0800

    修改教练开卡不能选择会员，更改UI

--- card/src/main/java/cn.qingchengfit.card/view/TrainerCardBuyFragment.java
+++ card/src/main/java/cn.qingchengfit.card/view/TrainerCardBuyFragment.java
@@ -11,9 +11,7 @@ public class TrainerCardBuyFragment extends StaffCardBuyFragment {
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View view = super.onCreateView(inflater, container, savedInstanceState);
-    civSaler.setShowRight(false);
     civSaler.setContent(loginStatus.getLoginUser().getUsername());
-    civSaler.setCanClick(false);
     //presenter.setSaler(loginStatus.getLoginUser().getId());
     return view;
   }
