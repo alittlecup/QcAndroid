c84ab58d87037a282d6eb59c5d7005cf0bb26c72
huangbaole@qingcheng.it <huangbaole>
2018-11-15 23:32:49 +0800

    修改6.0以下版本无法使用会员引导功能

--- saascommon/src/main/java/cn/qingchengfit/saascommon/widget/GuideView.java
+++ saascommon/src/main/java/cn/qingchengfit/saascommon/widget/GuideView.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
