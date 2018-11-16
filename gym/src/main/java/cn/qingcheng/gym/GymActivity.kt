16bcf6dd2746ea542abed338895b26c68a521e71
huangbaole@qingcheng.it <huangbaole>
2018-11-16 18:53:49 +0800

    添加 gym 模块

---
+++ gym/src/main/java/cn/qingcheng/gym/GymActivity.kt
@@ -0,0 +1,13 @@
+package cn.qingcheng.gym
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import cn.qingcheng.fit.gym.R
+
+class GymActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_gym)
+  }
+}
