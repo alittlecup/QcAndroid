aa9643fa42066e57e2c1d68784ddcf62e2101306
huangbaole@qingcheng.it <huangbaole>
2018-11-23 16:42:56 +0800

    修改会员卡有限期，支付方式空白 [id]84e8f6c4999a7cca6cac393a0d6f08da

--- saasbase/src/main/java/cn/qingchengfit/saasbase/cards/views/offday/PayMethodOfflineDialog.java
+++ saasbase/src/main/java/cn/qingchengfit/saasbase/cards/views/offday/PayMethodOfflineDialog.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -11,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-
 
 import cn.qingchengfit.RxBus;
 import cn.qingchengfit.items.SimpleTextItemItem;
@@ -48,57 +48,59 @@ import java.util.List;
  * Created by Paper on 2017/5/5.
  */
 
-public class PayMethodOfflineDialog extends BaseDialogFragment implements FlexibleAdapter.OnItemClickListener {
-	RecyclerView rv;
-    private CommonFlexAdapter mAdapter;
-    private List<AbstractFlexibleItem> mDatas = new ArrayList<>();
+public class PayMethodOfflineDialog extends BaseDialogFragment
+    implements FlexibleAdapter.OnItemClickListener {
+  RecyclerView rv;
+  private CommonFlexAdapter mAdapter;
+  private List<AbstractFlexibleItem> mDatas = new ArrayList<>();
 
-    @Nullable @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.bottom_wardrobe_pay, container, false);
-      rv = (RecyclerView) view.findViewById(R.id.rv);
+  @Nullable @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.bottom_wardrobe_pay, container, false);
+    rv = (RecyclerView) view.findViewById(R.id.rv);
 
-      rv.setHasFixedSize(true);
-        rv.setLayoutManager(new SmoothScrollLinearLayoutManager(getContext()));
-        mAdapter = new CommonFlexAdapter(mDatas, this);
-        mDatas.clear();
-        mDatas.add(new SimpleTextItemItem("线下支付"));
-        mDatas.add(new PayWardrobeItem(2, null));
-        mDatas.add(new PayWardrobeItem(4, null));
-        mDatas.add(new PayWardrobeItem(5, null));
-        mDatas.add(new PayWardrobeItem(6, null));
-        rv.setAdapter(mAdapter);
-        return view;
+    rv.setHasFixedSize(true);
+    rv.setLayoutManager(new SmoothScrollLinearLayoutManager(getContext()));
+    mAdapter = new CommonFlexAdapter(mDatas, this);
+    mDatas.clear();
+    mDatas.add(new SimpleTextItemItem("线下支付"));
+    mDatas.add(new PayWardrobeItem(2, null));
+    mDatas.add(new PayWardrobeItem(4, null));
+    mDatas.add(new PayWardrobeItem(5, null));
+    mDatas.add(new PayWardrobeItem(6, null));
+    rv.setAdapter(mAdapter);
+    mAdapter.updateDataSet(mDatas);
+    return view;
+  }
+
+  @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setStyle(DialogFragment.STYLE_NORMAL, R.style.ChoosePicDialogStyle);
+
+  }
+
+  @NonNull @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
+    Dialog dialog = super.onCreateDialog(savedInstanceState);
+    dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    Window window = dialog.getWindow();
+    window.getDecorView().setPadding(0, 0, 0, 0);
+
+    WindowManager.LayoutParams wlp = window.getAttributes();
+    wlp.gravity = Gravity.BOTTOM;
+    wlp.width = WindowManager.LayoutParams.MATCH_PARENT;
+    wlp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+    window.setAttributes(wlp);
+    window.setWindowAnimations(R.style.ButtomDialogStyle);
+    dialog.setCanceledOnTouchOutside(true);
+    return dialog;
+  }
+
+  @Override public boolean onItemClick(int i) {
+    if (mAdapter.getItem(i) instanceof PayWardrobeItem) {
+      RxBus.getBus().post(new PayEvent(((PayWardrobeItem) mAdapter.getItem(i)).getPay_mode()));
+      dismiss();
     }
-
-    @Override public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @NonNull @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
-        //        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        Dialog dialog = super.onCreateDialog(savedInstanceState);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        Window window = dialog.getWindow();
-        window.getDecorView().setPadding(0, 0, 0, 0);
-
-        WindowManager.LayoutParams wlp = window.getAttributes();
-        wlp.gravity = Gravity.BOTTOM;
-        wlp.width = WindowManager.LayoutParams.MATCH_PARENT;
-        wlp.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        window.setAttributes(wlp);
-        window.setWindowAnimations(R.style.ButtomDialogStyle);
-        dialog.setCanceledOnTouchOutside(true);
-
-        return dialog;
-    }
-
-    @Override public boolean onItemClick(int i) {
-        if (mAdapter.getItem(i) instanceof PayWardrobeItem) {
-            RxBus.getBus().post(new PayEvent(((PayWardrobeItem) mAdapter.getItem(i)).getPay_mode()));
-            dismiss();
-        }
-        return false;
-    }
+    return false;
+  }
 }
